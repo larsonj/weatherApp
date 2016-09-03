41de6191a7cc864f9aefc93cc08510bbe6113747
jcl.macBookPro17 <larsonj.home@gmail.com>
2016-09-03 11:26:41 -0400

    tabs wip

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
-import { ionicBootstrap, Platform } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import {Component} from "@angular/core";
+import {ionicBootstrap, Platform, Events, MenuController, Nav} from "ionic-angular";
+import {StatusBar, Splashscreen} from "ionic-native";
+import {HomePage} from "./pages/home/home";
 
-import { HomePage } from './pages/home/home';
 
+interface PageObj {
+  title: string;
+  component: any;
+  icon: string;
+  index?: number;
+}
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: 'build/app.html'
 })
-export class MyApp {
+export class WeatherTether {
+
+  @ViewChild(Nav) nav: Nav;
+
+  appPages: PageObj[] = [
+    { title: 'Account', component: TabsPage, icon: 'calendar' },
+    { title: 'About', component: TabsPage, icon: 'rain' }
+  ];
+
+  loggedInPages: PageObj[] = [
+    { title: 'Account', component: TabsPage, icon: 'calendar' },
+    { title: 'Logout', component: TabsPage, icon: 'rain' }
+  ];
+
+  loggeOutPages: PageObj[] = [
+    { title: 'Login', component: TabsPage, icon: 'calendar' },
+    { title: 'Signup', component: TabsPage, icon: 'rain' }
+  ];
+
   rootPage: any = HomePage;
 
   constructor(platform: Platform) {
@@ -16,10 +40,11 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
+      Splashscreen.hide();
     });
   }
 }
 console.log('app started');
-ionicBootstrap(MyApp);
+ionicBootstrap(WeatherTether);
 console.log('app ended');
 
