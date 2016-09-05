56a6bd6900952f8a4ec988950f44081913896102
John Larson <larsonj.home@gmail.com>
2016-09-05 18:00:14 -0400

    initial sidemenu code - wip

--- app/app.ts
+++ app/app.ts
@@ -1,22 +1,41 @@
-import { Component } from '@angular/core';
-import { ionicBootstrap, Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
 import { StatusBar } from 'ionic-native';
 import { TabsPage } from './pages/tabs/tabs';
+import { HomePage } from './pages/home/home';
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  //template: '<ion-nav [root]="rootPage"></ion-nav>'
+  templateUrl: './build/app.html'
 })
-export class MyApp {
-  rootPage: any = TabsPage;
+export class WeatherTether {
+  @ViewChild(Nav) nav:Nav;
 
-  constructor(platform: Platform) {
-    platform.ready().then(() => {
+  rootPage: any = HomePage; // todo: set HomePage
+
+  pages: Array<{title: string, component: any}>;
+
+  constructor(public platform: Platform) {
+    this.initializeApp();
+
+    this.pages = [
+      {title: "New Search", component: HomePage}
+    ];
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
   }
+
+  openPage(page) {
+    this.nav.setRoot(page.component);
+  }
 }
-ionicBootstrap(MyApp);
+
+ionicBootstrap(WeatherTether);
 
 
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Weather Tether
 
-A cross-platform App that lets you stay tethered to your local weather reports.
+A cross-platform App that lets you stay tethered to local weather.
