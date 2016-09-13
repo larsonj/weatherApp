583c598dd17f66c4031402b435ac2f37cff43566
john@macair <john@mygardeninsight.com>
2016-09-13 13:17:56 +0100

    tabs

--- app/app.ts
+++ app/app.ts
@@ -1,46 +1,46 @@
-import { Component, ViewChild } from '@angular/core';
-import { ionicBootstrap, Platform, Nav } from 'ionic-angular';
-import { StatusBar } from 'ionic-native';
+import {Component, ViewChild} from '@angular/core';
+import {ionicBootstrap, Platform, Nav} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
 
-import { HomePage } from './pages/home/home';
-import { SettingsPage } from './pages/settings/settings';
-import { AboutPage } from './pages/about/about';
+import {HomePage} from './pages/home/home';
+import {SettingsPage} from './pages/settings/settings';
+import {AboutPage} from './pages/about/about';
+import {TabsPage} from './pages/tabs/tabs';
 
 @Component({
-templateUrl: './app.html'
+  templateUrl: 'build/app.html'
 })
 class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-rootPage: any = TabsPage;
+  rootPage: any = TabsPage;
 
-pages: Array<{title: string, component: any}>;
+  pages: Array<{title: string, component: any}>;
 
-constructor(public platform: Platform) {
-this.initializeApp();
+  constructor(public platform: Platform) {
+    this.initializeApp();
 
 // used for an example of ngFor and navigation
-this.pages = [
-  { title: 'Home', component: HomePage },
-  { title: 'Settings', component: SettingsPage },
-  { title: 'About', component: AboutPage }
-];
+    this.pages = [
+      {title: 'Home', component: HomePage},
+      {title: 'Settings', component: SettingsPage},
+      {title: 'About', component: AboutPage}
+    ];
+  }
 
-}
-
-initializeApp() {
-this.platform.ready().then(() => {
+  initializeApp() {
+    this.platform.ready().then(() => {
 // Okay, so the platform is ready and our plugins are available.
 // Here you can do any higher level native things you might need.
-StatusBar.styleDefault();
-});
-}
+      StatusBar.styleDefault();
+    });
+  }
 
-openPage(page) {
+  openPage(page) {
 // Reset the content nav to have just this page
 // we wouldn't want the back button to show in this scenario
-this.nav.setRoot(page.component);
-}
+    this.nav.setRoot(page.component);
+  }
 }
 
 ionicBootstrap(MyApp);
